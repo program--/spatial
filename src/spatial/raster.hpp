924c5cf4b9a7671d3d550045d25034bf0d50939d
program-- <justin@justinsingh.me>
2023-10-07 00:36:34 -0700

    docs: missing licensing info

--- src/spatial/raster.hpp
+++ src/spatial/raster.hpp
@@ -1,6 +1,14 @@
+// spatial: a general-purpose geospatial library
+//
+// Copyright (c) 2023 Justin Singh-Mohudpur <justin@justinsingh.me>
+//
+// Use, modification and distribution is subject to the
+// Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
-#include <spatial/raster/extent.hpp>
+#include <spatial/raster/fwd.hpp>
 #include <spatial/raster/axis.hpp>
 #include <spatial/raster/cell.hpp>
 #include <spatial/raster/raster.hpp>
